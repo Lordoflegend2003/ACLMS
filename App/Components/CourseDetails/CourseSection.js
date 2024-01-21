55d2702de7b63a9eb9b176b6183bdedfce6a4ec4
B Pawan Kalyan <bpawankalyan.it2021@citchennai.net>
2024-01-21 22:31:16 +0530

    picsupdated

--- App/Components/CourseDetails/CourseSection.js
+++ App/Components/CourseDetails/CourseSection.js
@@ -5,11 +5,15 @@ import Colors from '../../Utils/Colors';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function CourseSection({ course }) {
+  const isNameLong = course.name.length > 20;
+
   return (
     <View style={{ padding: 7 }}>
-      <View style={{ display: "flex", flexDirection: 'row', justifyContent: "space-between" }}>
+      <View style={{ flexDirection: isNameLong ? 'column' : 'row', justifyContent: "space-between" }}>
         <Text style={{ fontFamily: "Outfit-ebold", fontSize: 20, padding: 6 }}>{course.name}</Text>
-        <Text style={{ fontFamily: "Outfit-ebold", fontSize: 20, padding: 6 }}>{course.courselevel}</Text>
+        {isNameLong ? (
+          <Text style={{ fontFamily: "Outfit-ebold", fontSize: 20, padding: 6 }}>{course.courselevel}</Text>
+        ) : null}
       </View>
       {course.banner && course.banner.url && (
         <Image style={{ width: Dimensions.get('screen').width * 0.86, height: 200, borderRadius: 20 }} source={{ uri: course.banner.url }} />
@@ -32,9 +36,7 @@ export default function CourseSection({ course }) {
         }}>Description:</Text>
         <Text style={{ fontFamily: "Outfit-lig", color: Colors.grey, lineHeight: 25 }}>{course.description?.markdown}</Text>
       </View>
-      <View style = {{display : "flex" , flexDirection : "row" , 
-      justifyContent  : 'space-between',
-      marginTop : 10}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
         <TouchableOpacity style={{
           padding: 5,
           backgroundColor: "black", // Change background color based on price
@@ -44,7 +46,7 @@ export default function CourseSection({ course }) {
           alignItems: 'center', // Center the text horizontally
           justifyContent: 'center', // Center the text vertically
         }}>
-          <Text style={{ color: Colors.WHITE , fontSize : 14 , fontFamily : "Outfit-ebold"}}>{course.price > 0 ? 'Enroll Now' : 'Enroll for Free'}</Text>
+          <Text style={{ color: Colors.WHITE, fontSize: 14, fontFamily: "Outfit-ebold" }}>{course.price > 0 ? 'Enroll Now' : 'Enroll for Free'}</Text>
         </TouchableOpacity>
         <TouchableOpacity style={{
           padding: 5,
@@ -55,7 +57,7 @@ export default function CourseSection({ course }) {
           alignItems: 'center', // Center the text horizontally
           justifyContent: 'center', // Center the text vertically
         }}>
-          <Text style={{ color: Colors.WHITE , fontSize : 14 , fontFamily : "Outfit-ebold"}}>Get Membership</Text>
+          <Text style={{ color: Colors.WHITE, fontSize: 14, fontFamily: "Outfit-ebold" }}>Get Membership</Text>
         </TouchableOpacity>
       </View>
     </View>
--- App/Screen/LoginScreen.js
+++ App/Screen/LoginScreen.js
@@ -2,8 +2,10 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import * as WebBrowser from "expo-web-browser";
 import { useOAuth } from "@clerk/clerk-expo";
-import pnimg from "../../assets/imgs/hand-with-white-screen-smartphone.jpg";
+import pnimg from "../../assets/imgs/perfect.png";
 import gg from "../../assets/imgs/google.png";
+import fb from "../../assets/imgs/fb.png";
+import gh from "../../assets/imgs/github.png";
 import Colors from '../Utils/Colors';
 import { useWarmUpBrowser } from '../../hooks/warmUpBrowser';
 WebBrowser.maybeCompleteAuthSession();
@@ -37,13 +39,13 @@ export default function LoginScreen() {
         style={{
           width: 500,
           height: 500,
-          resizeMode: "contain",  // Corrected property name
+          resizeMode: "center",  // Corrected property name
         }}
       />
       <View style={{
-        height: 400,
+        height: 500,
         width: 400,
-        marginTop: -60,
+        marginTop: -30,
         padding: 20,
         backgroundColor: Colors.BLACK
       }}>
@@ -86,6 +88,56 @@ export default function LoginScreen() {
             marginLeft: 20,
           }}
           >Login with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+        onPress={onPress}
+          style={{
+            backgroundColor: Colors.WHITE,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            justifyContent: "center",
+            padding: 10,
+            borderRadius: 99,
+            marginTop: 20,
+          }}>
+          <Image source={fb}
+            style={{ width: 30, height: 30 }}
+          />
+          <Text style={{
+            textAlign: 'center',
+            fontSize: 20,
+            color: Colors.BLACK,
+            fontFamily: 'Outfit-mid',
+            marginLeft: 10,
+          }}
+          >Login with Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+        onPress={onPress}
+          style={{
+            backgroundColor: Colors.WHITE,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            justifyContent: "center",
+            padding: 10,
+            borderRadius: 99,
+            marginTop: 20,
+          }}>
+          <Image source={gh}
+            style={{ width: 35, height: 35 }}
+          />
+          <Text style={{
+            textAlign: 'center',
+            fontSize: 20,
+            color: Colors.BLACK,
+            fontFamily: 'Outfit-mid',
+            marginLeft: 10,
+          }}
+          >Login with Github</Text>
         </TouchableOpacity>
       </View>
     </View>
